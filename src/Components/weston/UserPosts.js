53a94c21e4fe0f6c1ff472cfc834447b26425171
westonperkins <westonperkins5@gmail.com>
2021-08-17 14:20:31 -0400

    added loader

--- src/Components/weston/UserPosts.js
+++ src/Components/weston/UserPosts.js
@@ -15,6 +15,7 @@ const UserPosts = (props) => {
             headers: {"auth-token": localStorage.getItem("auth-token")}
         })
         .then(res => setUserCredentials(res.data))
+        // .then(res => console.log(res.data))
     }, [])
     
     const [ posts, setPosts ] = useState([])
@@ -30,15 +31,15 @@ const UserPosts = (props) => {
         <div className="userprof-container">
             <UserContext.Provider value={{ userCredentials, setUserCredentials }}>  
             <div>
+            {console.log(userCredentials + "uC")}
             {userCredentials.length > 0 && (<h3>{userCredentials[0].name}</h3>)}
             {userCredentials.length > 0 && (<p>@{userCredentials[0].username}</p>)}
             {userCredentials.length > 0 && (<p>Currently works at: <span>{userCredentials[0].company}</span> as a <span>{userCredentials[0].occupation}</span></p>)}
             {userCredentials.length > 0 && (<p>Works at: <span>{userCredentials[0].company}</span></p>)}
             {userCredentials.length > 0 && (<div className="profileIcons"><i className="material-icons">email</i><span>{userCredentials[0].email}</span></div>)}
             </div>
-            {console.log(userCredentials[0])}
             {console.log(props)}
-            {/* {console.log(props.match.params.user)} */}
+            {console.log(props.match.params.user)}
             {/* <h4>{props.match.params.user}'s Posts</h4>  */}
             {posts.map((post) => {
                 if(props.match.params.user === post.postedBy) {
